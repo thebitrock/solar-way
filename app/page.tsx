e16d03973a3267506da5d772c188dee77f649a8b
Roman Pohorilchuk <roman@pohorilchuk.com>
2025-02-16 19:45:20 +0200

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import TodoCreateForm from "@/ui-components/TodoCreateForm";
 import ManufacturerCreateForm from '@/ui-components/ManufacturerCreateForm';
 // import { ManufacturerSelectField } from './components/ManufacturerSelectField';
 
